b50e448c76a55da4e6bd100a6946ee57ec8589b1
peterkong94 <peterkong94@gmail.com>
2017-10-23 11:25:10 -0400

    merge_FIX

--- Blackbody_Target.ino
+++ Blackbody_Target.ino
@@ -1,52 +1,10 @@
 //this is the main source file 
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-<<<<<<< HEAD
-LiquidCrystal_I2C lcd(0x3F,20,4);  // set the LCD address to 0x3F for a 20 chars and 4 line display
-int heatPin = 9;           // the PWM pin the LED is attached to
-int coolPin = 10;    // how bright the LED is
-
-#include "user_input.h"
-
-=======
->>>>>>> 8dd340bc486ab0b566518ef705867e67830c2770
 #include <math.h>
 #include "max6675.h"
 #include "PID_v1.h"
 #include "PID_AutoTune_v0.h"
-<<<<<<< HEAD
-//Define Variables we'll be connecting to
-double Setpoint, Input, Output;
-//Specify the links and initial tuning parameters
-PID myPID(&Input, &Output, &Setpoint,8,15,3, DIRECT);
-
-void setup() {
-  // put your setup code here, to run once
-  //setup LCD display
-  LCDSetup();
-  pinMode(heatPin, OUTPUT);
-  pinMode(coolPin, OUTPUT);
-  prevTemp = getCurrentTemp();
-  thermoInit();
-  PIDSetup();
-
-  user_input_setup();
-  
-  Serial.begin(9600);
-}
-
-void loop() {
-
-  //Serial.print(digitalRead(TEMPUP_PIN));
-  //Serial.print("/");
-  dispSetTemp(setTemp);
-  // put your main code here, to run repeatedly:
-  //Serial.println(thermocouple.readCelsius());
-  currentTemp = thermocouple.readCelsius();  
-  delay(500); 
-  dispCurrentTemp(currentTemp);
-  PIDCalc(currentTemp, setTemp);
-=======
 
 // Values to increase readability
 // These values help indicate whether or not a button is pressed
@@ -113,71 +71,70 @@ PID myPID(&input, &output, &setpoint,TUNING_P,TUNING_I,TUNING_D, DIRECT);
 
 void setup() 
 {
-	// setup user input
-	userInputSetup();
-	//setup LCD display
-	LCDSetup();
-	//set TE-COOLER output pin 
-	pinMode(HEAT_PIN, OUTPUT);
-	pinMode(COOL_PIN, OUTPUT);
-	// initialize the thermocouples
-	thermoInit();  // what does this do? Do we only need it once no matter how many thermocouples there are?
-	// set the input and setpoint of the PID loop
-	pidSetup(thermocouple_blackbody);
+  // setup user input
+  userInputSetup();
+  //setup LCD display
+  LCDSetup();
+  //set TE-COOLER output pin 
+  pinMode(HEAT_PIN, OUTPUT);
+  pinMode(COOL_PIN, OUTPUT);
+  // initialize the thermocouples
+  thermoInit();  // what does this do? Do we only need it once no matter how many thermocouples there are?
+  // set the input and setpoint of the PID loop
+  pidSetup(thermocouple_blackbody);
 
-	// set up global variables
-	// temperatures set to predefined setpoint
-	setTemp = PREDEFINED_TEMP_SETPOINT;
-	absoluteTemp = PREDEFINED_TEMP_SETPOINT;
-	relativeTemp = 0;
-	// system mode initialized to absolute temperature
-	sysTempMode = ABS_TEMP;
-	prevTemp = thermocouple_blackbody.readCelsius();
+  // set up global variables
+  // temperatures set to predefined setpoint
+  setTemp = PREDEFINED_TEMP_SETPOINT;
+  absoluteTemp = PREDEFINED_TEMP_SETPOINT;
+  relativeTemp = 0;
+  // system mode initialized to absolute temperature
+  sysTempMode = ABS_TEMP;
+  prevTemp = thermocouple_blackbody.readCelsius();
 
-	// start serial
-	Serial.begin(9600);
+  // start serial
+  Serial.begin(9600);
 }
 
 void loop() {
-	
-	// local variables 
-	int error;       // difference between blackbody temperature and the setpoint
+  
+  // local variables 
+  int error;       // difference between blackbody temperature and the setpoint
         // the current temperature of the blackbody
-	double currentTemp = thermocouple_blackbody.readCelsius();
-	// current ambient temperature
-	int currentAmbientTemp = int(thermocouple_ambient.readCelsius());
+  double currentTemp = thermocouple_blackbody.readCelsius();
+  // current ambient temperature
+  int currentAmbientTemp = int(thermocouple_ambient.readCelsius());
         currentAmbientTemp = 25;
         
-	// get the user input for the temperature
-	userInputLoop(sysTempMode, relativeTemp, absoluteTemp); // values are passed by reference and modified
->>>>>>> 8dd340bc486ab0b566518ef705867e67830c2770
+  // get the user input for the temperature
+  userInputLoop(sysTempMode, relativeTemp, absoluteTemp); // values are passed by reference and modified
 
-	// depending on the user input set the setpoint
-	setSetpoint(setTemp, sysTempMode, relativeTemp, currentAmbientTemp, absoluteTemp);
+  // depending on the user input set the setpoint
+  setSetpoint(setTemp, sysTempMode, relativeTemp, currentAmbientTemp, absoluteTemp);
 
-	// display the temperature setpoint
-	dispSetTemp(setTemp);
+  // display the temperature setpoint
+  dispSetTemp(setTemp);
 
-	//Serial.println(thermocouple.readCelsius());
-	  
-	delay(500); 
+  //Serial.println(thermocouple.readCelsius());
+    
+  delay(500); 
 
-	// display the current blackbody temp
-	dispCurrentTemp(currentTemp);
+  // display the current blackbody temp
+  dispCurrentTemp(currentTemp);
 
-	pidCalc(currentTemp, setTemp);
+  pidCalc(currentTemp, setTemp);
 
-	// display the error to the LCD
-	error = currentTemp - setTemp;
-	dispError(error); 
+  // display the error to the LCD
+  error = currentTemp - setTemp;
+  dispError(error); 
   
-	// for averaging from last loop to reduce spikes, could be elimiated 
-	prevTemp = currentTemp; 
+  // for averaging from last loop to reduce spikes, could be elimiated 
+  prevTemp = currentTemp; 
 
-	// display the PWM value
-	disPWM(output);
+  // display the PWM value
+  disPWM(output);
 
-	// output the heating and cooling elements
-	tempCon(); 
+  // output the heating and cooling elements
+  tempCon(); 
   
 }
